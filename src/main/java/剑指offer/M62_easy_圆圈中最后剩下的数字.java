05fb8ffdc97e98df02619e4a5e07409c0b7e4501
alfredt <tangyu85@163.com>
2020-09-04 22:47:22 +0800

    9.4

---
+++ src/main/java/剑指offer/M62_easy_圆圈中最后剩下的数字.java
@@ -0,0 +1,64 @@
+package 剑指offer;
+
+/**
+ * @author : alfredt
+ * @version : 1.0.0
+ * @date : Created in 2020/9/4 10:33 上午
+ * @description : 剑指 Offer 62. 圆圈中最后剩下的数字
+ * @modified By  :
+ * <p>
+ * 0,1,,n-1这n个数字排成一个圆圈，从数字0开始，每次从这个圆圈里删除第m个数字。求出这个圆圈里剩下的最后一个数字。
+ * <p>
+ * 例如，0、1、2、3、4这5个数字组成一个圆圈，从数字0开始每次删除第3个数字，则删除的前4个数字依次是2、0、4、1，因此最后剩下的数字是3。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: n = 5, m = 3
+ * 输出: 3
+ * 示例 2：
+ * <p>
+ * 输入: n = 10, m = 17
+ * 输出: 2
+ *  
+ * <p>
+ * 限制：
+ * <p>
+ * 1 <= n <= 10^5
+ * 1 <= m <= 10^6
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class M62_easy_圆圈中最后剩下的数字 {
+
+    public int lastRemaining(int n, int m) {
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
