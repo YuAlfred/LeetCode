aae2b2cc313d74d6105f7a067b64a553bcc8fad1
alfredt <tangyu85@163.com>
2020-10-09 10:26:23 +0800

    每日

--- src/main/java/程序员面试经典/M17_05_middle_字母与数字.java
+++ src/main/java/程序员面试经典/M17_05_middle_字母与数字.java
@@ -1,5 +1,9 @@
 package 程序员面试经典;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author : alfredt
  * @version : 1.0.0
@@ -32,14 +36,45 @@ package 程序员面试经典;
 public class M17_05_middle_字母与数字 {
 
 
-//    public String[] findLongestSubarray(String[] array) {
-//        int[] iArray = new int[array.length];
-//        for (int i = 0; i < array.length; i++) {
-//            iArray[i] = array[i].charAt(0) <= '9' ? 1 : -1;
-//        }
-//
-//
-//    }
+    public static void main(String[] args) {
+        // String[] res = findLongestSubarray(new String[]{"A", "1", "B", "C", "D", "2", "3", "4", "E", "5", "F", "G", "6", "7", "H", "I", "J", "K", "L", "M"});
+        String[] res = findLongestSubarray(new String[]{"42", "10", "O", "t", "y", "p", "g", "B", "96", "H", "5", "v", "P", "52", "25", "96", "b", "L", "Y", "z", "d", "52", "3", "v", "71", "J", "A", "0", "v", "51", "E", "k", "H", "96", "21", "W", "59", "I", "V", "s", "59", "w", "X", "33", "29", "H", "32", "51", "f", "i", "58", "56", "66", "90", "F", "10", "93", "53", "85", "28", "78", "d", "67", "81", "T", "K", "S", "l", "L", "Z", "j", "5", "R", "b", "44", "R", "h", "B", "30", "63", "z", "75", "60", "m", "61", "a", "5", "S", "Z", "D", "2", "A", "W", "k", "84", "44", "96", "96", "y", "M"});
+        System.out.println(res);
+    }
+
+    public static String[] findLongestSubarray(String[] array) {
+        int[] iArray = new int[array.length];
+        iArray[0] = array[0].charAt(0) <= '9' ? 1 : -1;
+        for (int i = 1; i < array.length; i++) {
+            iArray[i] = array[i].charAt(0) <= '9' ? 1 : -1;
+            iArray[i] = iArray[i - 1] + iArray[i];
+        }
+        Map<Integer, Integer> location = new HashMap<>();
+        int start = 0, end = 0;
+        int max = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (iArray[i] == 0) {
+                int length = i + 1;
+                if (length > max) {
+                    max = length;
+                    start = 0;
+                    end = i + 1;
+                }
+                continue;
+            }
+            if (!location.containsKey(iArray[i])) {
+                location.put(iArray[i], i);
+            } else {
+                int length = i - location.get(iArray[i]);
+                if (length > max) {
+                    max = length;
+                    start = location.get(iArray[i]) + 1;
+                    end = i + 1;
+                }
+            }
+        }
+        return Arrays.copyOfRange(array, start, end);
+    }
 }
 
 
