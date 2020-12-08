22d9ce5b3b61deb69588fcd339ac122d3f3b823a
Alfred-mac <tangyu85@163.com>
2020-12-08 23:53:30 +0800

    每日

---
+++ src/main/java/每日打卡/M118_easy_杨辉三角.java
@@ -0,0 +1,67 @@
+package 每日打卡;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author alfredt
+ * @version 1.0.0
+ * @description: 118. 杨辉三角
+ * @date: 2020/12/6 4:12 下午
+ * 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+ * <p>
+ * <p>
+ * <p>
+ * 在杨辉三角中，每个数是它左上方和右上方的数的和。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 5
+ * 输出:
+ * [
+ * [1],
+ * [1,1],
+ * [1,2,1],
+ * [1,3,3,1],
+ * [1,4,6,4,1]
+ * ]
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/pascals-triangle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class M118_easy_杨辉三角 {
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numRows; ++i) {
+            List<Integer> row = new ArrayList<Integer>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(ret.get(i - 1).get(j - 1) + ret.get(i - 1).get(j));
+                }
+            }
+            ret.add(row);
+        }
+        return ret;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
