651489292f1e213f9b5df6236a7a463fe2e47e1e
Alfred-mac <tangyu85@163.com>
2021-02-18 12:37:22 +0800

    每日

--- src/main/java/每日打卡/M995_haed_K连续位的最小翻转次数.java
+++ src/main/java/每日打卡/M995_haed_K连续位的最小翻转次数.java
@@ -46,7 +46,7 @@ public class M995_haed_K连续位的最小翻转次数 {
 
         int res = 0, dif = 0;
         for (int i = 0; i < A.length; i++) {
-            if (((A[i] + dif) & 1) == 1) {
+            if (((A[i] + dif) & 1) == 0) {
                 if (i > A.length - K) {
                     return -1;
                 }
