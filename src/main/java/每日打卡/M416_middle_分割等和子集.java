4ec060a305b5a3dd33d00dd631e7f172a8087ccf
Alfred-mac <tangyu85@163.com>
2021-03-03 13:45:14 +0800

    每日

--- src/main/java/每日打卡/M416_middle_分割等和子集.java
+++ src/main/java/每日打卡/M416_middle_分割等和子集.java
@@ -34,7 +34,44 @@ package 每日打卡;
  */
 public class M416_middle_分割等和子集 {
 
+
     public boolean canPartition(int[] nums) {
+
+        if (nums.length < 2) {
+            return false;
+        }
+        int sum = 0;
+        int max = 0;
+        for (int num : nums) {
+            sum += num;
+            max = Math.max(max, num);
+        }
+        if ((sum & 1) == 1) {
+            return false;
+        }
+        sum = sum / 2;
+        if (max > sum) {
+            return false;
+        }
+        boolean[][] dp = new boolean[nums.length][sum + 1];
+        for (int i = 0; i < nums.length; i++) {
+            dp[i][0] = true;
+        }
+        dp[0][nums[0]] = true;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                if (j >= nums[i]) {
+                    dp[i][j] |= dp[i - 1][j - nums[i]];
+                }
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[nums.length - 1][sum];
+    }
+
+
+
+    /*public boolean canPartition(int[] nums) {
         int n = nums.length;
         if (n < 2) {
             return false;
@@ -67,5 +104,5 @@ public class M416_middle_分割等和子集 {
             }
         }
         return dp[n - 1][target];
-    }
+    }*/
 }
