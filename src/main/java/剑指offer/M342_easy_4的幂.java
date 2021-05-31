2bea707ea04af9387fffad8d85f89b20e3f60d76
Alfred-mac <tangyu85@163.com>
2021-05-31 09:53:39 +0800

    每日

---
+++ src/main/java/剑指offer/M342_easy_4的幂.java
@@ -0,0 +1,51 @@
+package 剑指offer;
+
+/**
+ * @author alfredt
+ * @version 1.0.0
+ * @description: 342. 4的幂
+ * @date: 2021/5/31 9:40 上午
+ * <p>
+ * 给定一个整数，写一个函数来判断它是否是 4 的幂次方。如果是，返回 true ；否则，返回 false 。
+ * <p>
+ * 整数 n 是 4 的幂次方需满足：存在整数 x 使得 n == 4x
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 16
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * 输入：n = 5
+ * 输出：false
+ * 示例 3：
+ * <p>
+ * 输入：n = 1
+ * 输出：true
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * -231 <= n <= 231 - 1
+ *  
+ * <p>
+ * 进阶：
+ * <p>
+ * 你能不使用循环或者递归来完成本题吗？
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/power-of-four
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class M342_easy_4的幂 {
+
+
+    public boolean isPowerOfFour(int n) {
+        return n > 0 && (n & (n - 1)) == 0 && (n & 0xaaaaaaaa) == 0;
+    }
+
+
+}
