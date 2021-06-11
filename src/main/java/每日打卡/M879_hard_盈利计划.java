6852f1f9330c441fb29ca0a49190e2c1e6765a4e
Alfred-mac <tangyu85@163.com>
2021-06-11 17:20:26 +0800

    每日

--- src/main/java/每日打卡/M879_hard_盈利计划.java
+++ src/main/java/每日打卡/M879_hard_盈利计划.java
@@ -53,20 +53,22 @@ public class M879_hard_盈利计划 {
     public int mod = 1000000007;
 
     public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
-        int[][] dp = new int[n + 1][minProfit + 1];
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = 1;
+        int m = group.length;
+        int[][][] dp = new int[m + 1][n + 1][minProfit + 1];
+        for (int i = 0; i <=n; i++) {
+            dp[0][i][0] =1;
         }
-        int len = group.length, MOD = (int)1e9 + 7;
-        for (int i = 1; i <= len; i++) {
-            int members = group[i - 1], earn = profit[i - 1];
-            for (int j = n; j >= members; j--) {
-                for (int k = minProfit; k >= 0; k--) {
-                    dp[j][k] = (dp[j][k] + dp[j - members][Math.max(0, k - earn)]) % MOD;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                for (int k = 0; k <= minProfit; k++) {
+                    dp[i][j][k] = dp[i - 1][j][k];
+                    if (j - group[i - 1] >= 0) {
+                        dp[i][j][k] = (dp[i][j][k] + dp[i - 1][j - group[i - 1]][Math.max(0, k - profit[i - 1])]) % mod;
+                    }
                 }
             }
         }
-        return dp[n][minProfit];
+        return dp[m][n][minProfit];
     }
 
 }
