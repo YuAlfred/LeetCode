bad4cc7a68bb8a30e2102c7b0169a2fe305357d9
Alfred-mac <tangyu85@163.com>
2021-06-14 22:36:48 +0800

    每日

--- src/main/java/每日打卡/M374_easy_猜数字大小.java
+++ src/main/java/每日打卡/M374_easy_猜数字大小.java
@@ -78,4 +78,21 @@ public class M374_easy_猜数字大小 {
     }
 
 
+    public int guessNumber2(int n) {
+
+        int l = 1, r = n;
+        while (l < r) {
+            int m = (l + (r - l)) / 2;
+            int g = guess(m);
+            if (g == 0) {
+                return m;
+            } else if (g == 1) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return l;
+    }
+
 }
