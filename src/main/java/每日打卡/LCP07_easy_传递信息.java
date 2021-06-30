11bf3d3ad3fd8af4952d30adc47444c82555d38b
Alfred-mac <tangyu85@163.com>
2021-07-01 00:21:34 +0800

    Default Changelist

---
+++ src/main/java/每日打卡/LCP07_easy_传递信息.java
@@ -0,0 +1,76 @@
+package 每日打卡;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ * @author alfredt
+ * @version 1.0.0
+ * @description: LCP 07. 传递信息
+ * @date: 2021/7/1 12:05 上午
+ * 小朋友 A 在和 ta 的小伙伴们玩传信息游戏，游戏规则如下：
+ * <p>
+ * 有 n 名玩家，所有玩家编号分别为 0 ～ n-1，其中小朋友 A 的编号为 0
+ * 每个玩家都有固定的若干个可传信息的其他玩家（也可能没有）。传信息的关系是单向的（比如 A 可以向 B 传信息，但 B 不能向 A 传信息）。
+ * 每轮信息必须需要传递给另一个人，且信息可重复经过同一个人
+ * 给定总玩家数 n，以及按 [玩家编号,对应可传递玩家编号] 关系组成的二维数组 relation。返回信息从小 A (编号 0 ) 经过 k 轮传递到编号为 n-1 的小伙伴处的方案数；若不能到达，返回 0。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 5, relation = [[0,2],[2,1],[3,4],[2,3],[1,4],[2,0],[0,4]], k = 3
+ * <p>
+ * 输出：3
+ * <p>
+ * 解释：信息从小 A 编号 0 处开始，经 3 轮传递，到达编号 4。共有 3 种方案，分别是 0->2->0->4， 0->2->1->4， 0->2->3->4。
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：n = 3, relation = [[0,2],[2,1]], k = 2
+ * <p>
+ * 输出：0
+ * <p>
+ * 解释：信息不能从小 A 处经过 2 轮传递到编号 2
+ * <p>
+ * 限制：
+ * <p>
+ * 2 <= n <= 10
+ * 1 <= k <= 5
+ * 1 <= relation.length <= 90, 且 relation[i].length == 2
+ * 0 <= relation[i][0],relation[i][1] < n 且 relation[i][0] != relation[i][1]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/chuan-di-xin-xi
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LCP07_easy_传递信息 {
+
+    public int ans = 0;
+
+    public int numWays(int n, int[][] relations, int k) {
+        boolean[][] graph = new boolean[n][n];
+        for (int[] relation : relations) {
+            graph[relation[0]][relation[1]] = true;
+        }
+        dfs(n, graph, k, 0);
+        return ans;
+    }
+
+    public void dfs(int n, boolean[][] graph, int k, int i) {
+        if (k == 0 && i == n - 1) {
+            ans++;
+        }
+        if (k == 0) {
+            return;
+        }
+        boolean[] relation = graph[i];
+        for (int j = 0; j < relation.length; j++) {
+            if (relation[j]) {
+                dfs(n, graph, k - 1, j);
+            }
+        }
+    }
+
+
+}
