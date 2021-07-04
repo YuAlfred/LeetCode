2ac46044dfa237d1baadf3000145e2140ff6191d
Alfred-mac <tangyu85@163.com>
2021-07-05 00:01:28 +0800

    每日

---
+++ src/main/java/每日打卡/M451_middle_根据字符出现频率排序.java
@@ -0,0 +1,76 @@
+package 每日打卡;
+
+import java.util.*;
+
+/**
+ * @author alfredt
+ * @version 1.0.0
+ * @description: 451. 根据字符出现频率排序
+ * @date: 2021/7/3 12:08 上午
+ * 给定一个字符串，请将字符串里的字符按照出现的频率降序排列。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入:
+ * "tree"
+ * <p>
+ * 输出:
+ * "eert"
+ * <p>
+ * 解释:
+ * 'e'出现两次，'r'和't'都只出现一次。
+ * 因此'e'必须出现在'r'和't'之前。此外，"eetr"也是一个有效的答案。
+ * 示例 2:
+ * <p>
+ * 输入:
+ * "cccaaa"
+ * <p>
+ * 输出:
+ * "cccaaa"
+ * <p>
+ * 解释:
+ * 'c'和'a'都出现三次。此外，"aaaccc"也是有效的答案。
+ * 注意"cacaca"是不正确的，因为相同的字母必须放在一起。
+ * 示例 3:
+ * <p>
+ * 输入:
+ * "Aabb"
+ * <p>
+ * 输出:
+ * "bbAa"
+ * <p>
+ * 解释:
+ * 此外，"bbaA"也是一个有效的答案，但"Aabb"是不正确的。
+ * 注意'A'和'a'被认为是两种不同的字符。
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sort-characters-by-frequency
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class M451_middle_根据字符出现频率排序 {
+
+    public String frequencySort(String s) {
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            int val = map.getOrDefault(c, 0);
+            map.put(c, val + 1);
+        }
+        StringBuilder sb = new StringBuilder();
+        map.entrySet().stream().sorted(new Comparator<Map.Entry<Character, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Character, Integer> o1, Map.Entry<Character, Integer> o2) {
+                return o2.getValue() - o1.getValue();
+            }
+        }).forEachOrdered(characterIntegerEntry -> {
+            for (Integer value = characterIntegerEntry.getValue(); value > 0; value--) {
+                sb.append(characterIntegerEntry.getKey());
+            }
+        });
+        return sb.toString();
+
+    }
+
+
+}
---
+++ src/main/java/每日打卡/M645_easy_错误的集合.java
@@ -0,0 +1,58 @@
+package 每日打卡;
+
+/**
+ * @author alfredt
+ * @version 1.0.0
+ * @description: 645. 错误的集合
+ * @date: 2021/7/4 10:32 上午
+ * 集合 s 包含从 1 到 n 的整数。不幸的是，因为数据错误，导致集合里面某一个数字复制了成了集合里面的另外一个数字的值，导致集合 丢失了一个数字 并且 有一个数字重复 。
+ * <p>
+ * 给定一个数组 nums 代表了集合 S 发生错误后的结果。
+ * <p>
+ * 请你找出重复出现的整数，再找到丢失的整数，将它们以数组的形式返回。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,2,2,4]
+ * 输出：[2,3]
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1,1]
+ * 输出：[1,2]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 2 <= nums.length <= 104
+ * 1 <= nums[i] <= 104
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/set-mismatch
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class M645_easy_错误的集合 {
+
+    public int[] findErrorNums(int[] nums) {
+        int[] ans = new int[2];
+        boolean[] newNums = new boolean[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int cur = nums[i];
+            if (newNums[cur]) {
+                ans[0] = cur;
+            } else {
+                newNums[cur] = true;
+            }
+        }
+        for (int i = 0; i < newNums.length; i++) {
+            if (!newNums[i]) {
+                ans[1] = i;
+                return ans;
+            }
+        }
+        return null;
+    }
+
+
+}
