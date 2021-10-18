260003bb70daba3fd86723fc20298cf991173f49
Alfred-mac <tangyu85@163.com>
2021-10-18 19:45:56 +0800

    每日

---
+++ src/main/java/每日打卡/M476_easy_数字的补数.java
@@ -0,0 +1,51 @@
+package 每日打卡;
+
+/**
+ * @author alfredt
+ * @version 1.0.0
+ * @description: 476. 数字的补数
+ * @date: 2021/10/18 6:38 下午
+ * 对整数的二进制表示取反（0 变 1 ，1 变 0）后，再转换为十进制表示，可以得到这个整数的补数。
+ * <p>
+ * 例如，整数 5 的二进制表示是 "101" ，取反后得到 "010" ，再转回十进制表示得到补数 2 。
+ * 给你一个整数 num ，输出它的补数。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：num = 5
+ * 输出：2
+ * 解释：5 的二进制表示为 101（没有前导零位），其补数为 010。所以你需要输出 2 。
+ * 示例 2：
+ * <p>
+ * 输入：num = 1
+ * 输出：0
+ * 解释：1 的二进制表示为 1（没有前导零位），其补数为 0。所以你需要输出 0 。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= num < 231
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/number-complement
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class M476_easy_数字的补数 {
+
+    public int findComplement(int num) {
+        int highbit = 0;
+        for (int i = 1; i <= 30; ++i) {
+            if (num >= 1 << i) {
+                highbit = i;
+            } else {
+                break;
+            }
+        }
+        int mask = highbit == 30 ? 0x7fffffff : (1 << (highbit + 1)) - 1;
+        return num ^ mask;
+    }
+
+}
