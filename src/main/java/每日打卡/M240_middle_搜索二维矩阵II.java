2c2643b408377ea80c8fae0add10346e1560a183
Alfred-mac <tangyu85@163.com>
2021-10-26 00:33:40 +0800

    每日

---
+++ src/main/java/每日打卡/M240_middle_搜索二维矩阵II.java
@@ -0,0 +1,59 @@
+package 每日打卡;
+
+/**
+ * @author alfredt
+ * @version 1.0.0
+ * @description: 240. 搜索二维矩阵 II
+ * @date: 2021/10/25 9:15 下午
+ * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+ * <p>
+ * 每行的元素从左到右升序排列。
+ * 每列的元素从上到下升序排列。
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+ * 输出：false
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= n, m <= 300
+ * -109 <= matrix[i][j] <= 109
+ * 每行的所有元素从左到右升序排列
+ * 每列的所有元素从上到下升序排列
+ * -109 <= target <= 109
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-a-2d-matrix-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class M240_middle_搜索二维矩阵II {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int i = 0, j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return false;
+    }
+
+
+}
---
+++ src/main/java/每日打卡/M496_easy_下一个更大元素I.java
@@ -0,0 +1,71 @@
+package 每日打卡;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * @author alfredt
+ * @version 1.0.0
+ * @description: 496. 下一个更大元素 I
+ * @date: 2021/10/26 12:13 上午
+ * 给你两个 没有重复元素 的数组 nums1 和 nums2 ，其中nums1 是 nums2 的子集。
+ * <p>
+ * 请你找出 nums1 中每个元素在 nums2 中的下一个比其大的值。
+ * <p>
+ * nums1 中数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素。如果不存在，对应位置输出 -1 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums1 = [4,1,2], nums2 = [1,3,4,2].
+ * 输出: [-1,3,-1]
+ * 解释:
+ * 对于 num1 中的数字 4 ，你无法在第二个数组中找到下一个更大的数字，因此输出 -1 。
+ * 对于 num1 中的数字 1 ，第二个数组中数字1右边的下一个较大数字是 3 。
+ * 对于 num1 中的数字 2 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
+ * 示例 2:
+ * <p>
+ * 输入: nums1 = [2,4], nums2 = [1,2,3,4].
+ * 输出: [3,-1]
+ * 解释:
+ *     对于 num1 中的数字 2 ，第二个数组中的下一个较大数字是 3 。
+ * 对于 num1 中的数字 4 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums1.length <= nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 104
+ * nums1和nums2中所有整数 互不相同
+ * nums1 中的所有整数同样出现在 nums2 中
+ *  
+ * <p>
+ * 进阶：你可以设计一个时间复杂度为 O(nums1.length + nums2.length) 的解决方案吗？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/next-greater-element-i
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class M496_easy_下一个更大元素I {
+
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+        LinkedList<Integer> deque = new LinkedList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!deque.isEmpty() && deque.peek() < nums2[i]) {
+                deque.poll();
+            }
+            map.put(nums2[i], deque.isEmpty() ? -1 : deque.peek());
+            deque.push(nums2[i]);
+        }
+        for (int i = 0; i < nums1.length; i++) {
+            nums1[i] = map.getOrDefault(nums1[i], -1);
+        }
+        return nums1;
+    }
+
+
+}
