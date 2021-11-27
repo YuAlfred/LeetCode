56a77b6204d4cd7c59f333ce8c77282a6ef7886c
ty-AMD <731432318@qq.com>
2021-11-27 15:07:20 +0800

    每日

---
+++ src/main/java/每日打卡/M519_middle_随机翻转矩阵.java
@@ -0,0 +1,81 @@
+package 每日打卡;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * @author : Alfred
+ * @version : 1.0.0
+ * @date : Created in 2021/11/27 0:21
+ * @description : 519. 随机翻转矩阵
+ * 给你一个 m x n 的二元矩阵 matrix ，且所有值被初始化为 0 。请你设计一个算法，随机选取一个满足 matrix[i][j] == 0 的下标 (i, j) ，并将它的值变为 1 。所有满足 matrix[i][j] == 0 的下标 (i, j) 被选取的概率应当均等。
+ * <p>
+ * 尽量最少调用内置的随机函数，并且优化时间和空间复杂度。
+ * <p>
+ * 实现 Solution 类：
+ * <p>
+ * Solution(int m, int n) 使用二元矩阵的大小 m 和 n 初始化该对象
+ * int[] flip() 返回一个满足 matrix[i][j] == 0 的随机下标 [i, j] ，并将其对应格子中的值变为 1
+ * void reset() 将矩阵中所有的值重置为 0
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * 输入
+ * ["Solution", "flip", "flip", "flip", "reset", "flip"]
+ * [[3, 1], [], [], [], [], []]
+ * 输出
+ * [null, [1, 0], [2, 0], [0, 0], null, [2, 0]]
+ * <p>
+ * 解释
+ * Solution solution = new Solution(3, 1);
+ * solution.flip();  // 返回 [1, 0]，此时返回 [0,0]、[1,0] 和 [2,0] 的概率应当相同
+ * solution.flip();  // 返回 [2, 0]，因为 [1,0] 已经返回过了，此时返回 [2,0] 和 [0,0] 的概率应当相同
+ * solution.flip();  // 返回 [0, 0]，根据前面已经返回过的下标，此时只能返回 [0,0]
+ * solution.reset(); // 所有值都重置为 0 ，并可以再次选择下标返回
+ * solution.flip();  // 返回 [2, 0]，此时返回 [0,0]、[1,0] 和 [2,0] 的概率应当相同
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= m, n <= 104
+ * 每次调用flip 时，矩阵中至少存在一个值为 0 的格子。
+ * 最多调用 1000 次 flip 和 reset 方法。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/random-flip-matrix
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class M519_middle_随机翻转矩阵 {
+
+
+    class Solution {
+
+        Map<Integer, Integer> map;
+        int m, n;
+        int limit;
+        Random random = new Random();
+
+        public Solution(int m, int n) {
+            map = new HashMap<>();
+            this.m = m;
+            this.n = n;
+            this.limit = m * n;
+        }
+
+        public int[] flip() {
+            int rand = random.nextInt(limit--);
+            int idx = map.getOrDefault(rand, rand);
+            map.put(rand, map.getOrDefault(limit, limit));
+            return new int[]{idx / n, idx % n};
+        }
+
+        public void reset() {
+            limit = m * n;
+            map.clear();
+        }
+    }
+
+
+}
