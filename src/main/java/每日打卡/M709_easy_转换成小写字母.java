5892b1715d789e7ef4c51a8291152edbf8993202
ty-AMD <731432318@qq.com>
2021-12-12 23:07:28 +0800

    每日

---
+++ src/main/java/每日打卡/M709_easy_转换成小写字母.java
@@ -0,0 +1,42 @@
+package 每日打卡;
+
+/**
+ * @author : Alfred.T
+ * @version : 1.0.0
+ * @date : Created in 2021/12/12 23:06
+ * @description : 709. 转换成小写字母
+ * 给你一个字符串 s ，将该字符串中的大写字母转换成相同的小写字母，返回新的字符串。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "Hello"
+ * 输出："hello"
+ * 示例 2：
+ * <p>
+ * 输入：s = "here"
+ * 输出："here"
+ * 示例 3：
+ * <p>
+ * 输入：s = "LOVELY"
+ * 输出："lovely"
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 100
+ * s 由 ASCII 字符集中的可打印字符组成
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/to-lower-case
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class M709_easy_转换成小写字母 {
+
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+
+
+}
